81ff27f1ed48fbf0b5f395e894e1466f01c6558e
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-07 16:15:13 +0900

    training_js_14

---
+++ javascript/8_kyu/training_js_14.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57238ceaef9008adc7000603
+
+function colorOf(r,g,b){
+  r.toString(16).length < 2 ? r = '0' + r.toString(16) : r = r.toString(16);
+  g.toString(16).length < 2 ? g = '0' + g.toString(16) : g = g.toString(16);
+  b.toString(16).length < 2 ? b = '0' + b.toString(16) : b = b.toString(16);
+
+  return '#' + r + g + b;
+}
